199c099fd6affdc567e3eddfb809f343689ff745
jexiee99 <jelitaanggeraini73@gmail.com>
2025-10-10 00:37:58 +0700

    Upload all project

---
+++ project/lab-view/routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DemoController;
+
+Route::get('/hello', [DemoController::class, 'hello']);
+Route::get('/greet/{name}', [DemoController::class, 'greet']);
+Route::get('/search', [DemoController::class, 'search']);
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
